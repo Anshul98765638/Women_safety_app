a399277656107f6b36dbb7656a32c9569ea1fcfd
Anshul <anshuljindal1122@gmail.com>
2025-02-11 11:31:04 +0530

    Initial commit

---
+++ app/src/main/java/com/dan/naari/DeveloperByActivity.java
@@ -0,0 +1,13 @@
+package com.dan.naari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class DeveloperByActivity extends AppCompatActivity {
+
+
+}
